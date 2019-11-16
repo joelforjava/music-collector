b1840ebde13d682e37158cb7ba7ba60a208146cd
Joel Cochran <joelforjava@outlook.com>
2019-11-16 10:59:16 -0500

    New Song specs

--- src/main/java/com/joelforjava/music/service/SongFileService.java
+++ src/main/java/com/joelforjava/music/service/SongFileService.java
@@ -2,6 +2,8 @@ package com.joelforjava.music.service;
 
 import com.joelforjava.music.model.Song;
 
+import java.util.List;
+
 /**
  * Handles the loading of song data from a given file or files and the updating of song data to a given file or files.
  *
@@ -15,6 +17,14 @@ public interface SongFileService {
      * @return the song data for the requested file.
      */
     Song load(String filePath);
+
+    /**
+     * Load the song data for all songs that exist in the given directory path.
+     *
+     * @param directoryPath - a valid file path that points to a directory of music files
+     * @return the song data for all files within the provided directory path.
+     */
+    List<Song> loadAll(String directoryPath);
 
     /**
      * Update the tag data for a given song in the file system.
--- src/test/groovy/com/joelforjava/music/model/SongSpec.groovy
+++ src/test/groovy/com/joelforjava/music/model/SongSpec.groovy
@@ -1,6 +1,7 @@
 package com.joelforjava.music.model
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class SongSpec extends Specification {
 
@@ -17,4 +18,57 @@ class SongSpec extends Specification {
         song.title != null
         song.title == 'Imma MP3'
     }
+
+    void 'MP3 file will identify itself as an MP3 encoded file'() {
+        given: 'We have a file reference'
+        File file = new File(classLoader.getResource('empty.mp3').file)
+
+        when: 'We try to load it into a song object'
+        Song song = new Song(file)
+
+        then: 'The song will identify as an MP3'
+        song.encodingType == 'mp3'
+
+    }
+
+    @Unroll("File #fileName has an encoding type of #expectedEncodingType")
+    void 'Differently encoded files will identify as expected'() {
+        given: 'We have a file reference'
+        File file = new File(classLoader.getResource(fileName).file)
+
+        when: 'We try to load it into a song object'
+        Song song = new Song(file)
+
+        then: 'The song will report the expected encoding type'
+        song.encodingType == expectedEncodingType
+
+        where:
+        fileName         || expectedEncodingType
+        'empty.mp3'      || 'mp3'
+        'empty.flac'     || 'FLAC 16 bits'
+        'empty.ogg'      || 'Ogg Vorbis v1'
+        'empty.wma'      || 'ASF (audio): 0x0162 (Windows Media Audio 9 series (Professional))' // Yikes
+        'empty-alac.m4a' || 'Apple Lossless'
+    }
+
+    @Unroll("File #fileName has a format of #expectedFormat")
+    void 'Differently formatted files will identify as expected'() {
+        given: 'We have a file reference'
+        File file = new File(classLoader.getResource(fileName).file)
+
+        when: 'We try to load it into a song object'
+        Song song = new Song(file)
+
+        then: 'The song will report the expected format'
+        song.format == expectedFormat
+
+        where:
+        fileName         || expectedFormat
+        'empty.mp3'      || 'MPEG-1 Layer 3'
+        'empty.flac'     || 'FLAC 16 bits'
+        'empty.ogg'      || 'Ogg Vorbis v1'
+        'empty.wma'      || 'ASF (audio): 0x0162 (Windows Media Audio 9 series (Professional))' // Yikes
+        'empty-alac.m4a' || 'Apple Lossless'
+    }
+
 }
